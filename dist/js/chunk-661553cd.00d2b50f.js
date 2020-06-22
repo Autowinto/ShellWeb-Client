dfc10e2cd36446817e6feda1428748533474d39f
Autowinto <happymansi@hotmail.com>
2020-06-22 16:08:16 +0200

    Added /dist for testing purposes

---
+++ dist/js/chunk-661553cd.00d2b50f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-661553cd"],{dec9:function(n,c,w){}}]);
+//# sourceMappingURL=chunk-661553cd.00d2b50f.js.map
---
+++ dist/js/chunk-76e9da5d.3328c9ac.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-76e9da5d"],{e009:function(n,w,o){}}]);
+//# sourceMappingURL=chunk-76e9da5d.3328c9ac.js.map
