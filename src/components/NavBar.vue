eb442faf9a8ff2befe1997dfc7919e9ef1190b35
Magnus Christian Larsen <macl@itconfidence.dk>
2021-03-08 13:00:58 +0100

    Changed the copyright text to say 2021 rather than 2020

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -201,7 +201,7 @@
       <footer class="bg-white sticky-footer">
         <div class="container my-auto">
           <div class="text-center my-auto copyright">
-            <span>Copyright © IT Confidence A/S - 2020</span>
+            <span>Copyright © IT Confidence A/S - 2021</span>
           </div>
         </div>
       </footer>
