0c73f140291460f631263712f6bffdd722351114
Magnus Christian Larsen <macl@itconfidence.dk>
2021-04-09 14:28:52 +0200

    Removed a few unused components

--- src/components/LineChart.vue
+++
@@ -1,8 +0,0 @@
-<script>
-import {Line} from 'vue-chartjs'
-
-export default {
-  extends: Line,
-
-}
-</script>
--- src/components/views/Dashboard.vue
+++ src/components/views/Dashboard.vue
@@ -157,7 +157,6 @@
                   </div>
                 </div>
                 <div class="card-body">
-                  <LineChart></LineChart>
                   <div class="chart-area">
                     <canvas
                       data-bs-chart='{"type":"line","data":{"labels":["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"],"datasets":[{"label":"Earnings","fill":true,"data":["0","10000","5000","15000","10000","20000","15000","25000"],"backgroundColor":"rgba(78, 115, 223, 0.05)","borderColor":"rgba(78, 115, 223, 1)"}]},"options":{"maintainAspectRatio":false,"legend":{"display":false},"title":{},"scales":{"xAxes":[{"gridLines":{"color":"rgb(234, 236, 244)","zeroLineColor":"rgb(234, 236, 244)","drawBorder":false,"drawTicks":false,"borderDash":["2"],"zeroLineBorderDash":["2"],"drawOnChartArea":false},"ticks":{"fontColor":"#858796","padding":20}}],"yAxes":[{"gridLines":{"color":"rgb(234, 236, 244)","zeroLineColor":"rgb(234, 236, 244)","drawBorder":false,"drawTicks":false,"borderDash":["2"],"zeroLineBorderDash":["2"]},"ticks":{"fontColor":"#858796","padding":20}}]}}}'
@@ -432,11 +431,7 @@
 </template>
 
 <script>
-import LineChart from '../LineChart.vue'
-
 export default {
-  components: {
-    LineChart,
-  },
+  components: {},
 }
 </script>
