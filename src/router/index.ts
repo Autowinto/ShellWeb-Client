33d5c530e8208a50e9bf55e7a680fb042b71bec5
Magnus Christian Larsen <macl@itconfidence.dk>
2021-06-24 16:13:30 +0200

    Hotfix

--- src/router/index.ts
+++ src/router/index.ts
@@ -148,9 +148,11 @@ const router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
-  document.title =
-    to.meta.title + ' - IT-Confidence Shell' || 'Something Went Wrong'
-  next()
+  if (to) {
+    document.title =
+      to.meta.title + ' - IT-Confidence Shell' || 'Something Went Wrong'
+    next()
+  }
 })
 
 export default router
